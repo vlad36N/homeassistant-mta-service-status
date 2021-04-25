7ed13fae9bcf034e8324eb96ab5681548b4b0f1b
Vladislav Nechayev <vladikn36@gmail.com>
2021-04-25 17:07:25 -0400

    Create service_status.py

---
+++ python_scripts/service_status.py
@@ -0,0 +1,47 @@
+import urllib
+import urllib.request
+import xml.etree.ElementTree as ET
+import json
+
+What_To_Display = 1
+url = "http://web.mta.info/status/serviceStatus.txt"
+
+def Status():
+    message = SubwayStatus()
+    return message
+
+def SubwayStatus():
+    with urllib.request.urlopen(url) as response:
+         html = response.read()
+         response.close()
+         message_all = ""
+         new_data = []
+         if What_To_Display == 1:
+             currentStatus = {}
+             root = ET.fromstring(html)
+             for i in range(2, 7):
+                 m = ""
+                 ##ent_name = ""
+                 if i == 2:
+                         m = "Subway Status:"
+                 elif i == 3:
+                         m = "Bus Status:"
+                 elif i == 4:
+                         m = "BT Status:"
+                 elif i == 5:
+                         m = "LIRR Status:"
+                 elif i == 6:
+                         m = "MetroNorth Status:"
+                 for child in root[i]:
+                     currentStatus[child[0].text] = child[1].text
+                 for k,v in currentStatus.items():
+                     new_data.append({"title": k, "status": v})
+                 container = {}
+                 container['items'] = new_data
+                 with open('/config/data/MTA_' + str(i) + '_data.json', 'w') as outfile: json.dump(container, outfile)
+                 container = {}
+                 new_data = []
+                 currentStatus = {}
+    return message_all
+
+Status()
